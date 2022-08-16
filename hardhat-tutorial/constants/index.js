070a608734f024d0fa23de31c2480dd4d85b86be
Sergey Glibey <sergey.glibey@gmail.com>
2022-08-16 12:22:35 +0300

    --ICO dapp example

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x369cfC98b547Ce4668AdE956bDAf73bC4A1d6Cd6";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
